7e826f35013ac2dd88b4cf8dfb11efb4af790886
ANANTHA1628 <aryaputraferdiananta@gmail.com>
2024-10-03 08:36:33 +0700

    jobsheet6

--- Pemilihan2Percobaan106.java
+++ Pemilihan2Percobaan106.java
@@ -3,19 +3,16 @@ public class Pemilihan2Percobaan106 {
 
     public static void main(String[] args) {
         Scanner input06 =new Scanner(System.in);
-
         int tahun;
-
-        System.out.println("Masukkan tahun: ");
+        System.out.print("masukkan tahun : ");
         tahun = input06.nextInt();
 
-        if ((tahun % 4)== 0) {
-            if ((tahun % 100) !=0) 
-                System.out.println("Tahun Kabisat");
-        } else
+        if ((tahun % 4) == 0) {
+         if ((tahun %100) !=0) 
+            System.out.println("Tahun Kabisat");
+
+        }else 
             System.out.println("Bukan Tahun Kabisat");
 
-           input06.close(); 
-        }
-    
     }
+}
